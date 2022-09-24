9b4e73cee9e933070be998747bf4489a336c6e81
Leonid <kissmykrabeskill@gmail.com>
2022-09-24 22:19:01 +0500

    Read and update score student

--- src/main/java/home/des/example/lesson1/DatabaseMethods.java
+++ src/main/java/home/des/example/lesson1/DatabaseMethods.java
@@ -15,20 +15,20 @@ public class DatabaseMethods {
     public static void main(String[] args) {
         try {
             connect();
+//            insertNewStudent(25, "Bob1");
+//            insertNewStudent(26, "Bob2");
+//            insertNewStudent(27, "Bob3");
+//            insertNewStudent(28, "Bob4");
 //            findAllStudents();
-//            insertNewStudent(28, "Ann");
-//            findAllStudents();
-//            updateStudent(3, 29, "Ann");
-//            findAllStudents();
-//            deleteStudent(2);
-//            findAllStudents();
-        readAndSetNewStudentScore("students.txt");
+            readAndSetNewStudentScore("students.txt");
+            findAllStudents();
         } catch (SQLException | FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
     private static void connect() throws SQLException {
+
         connection = DriverManager.getConnection("jdbc:postgresql://localhost/students", "postgres", "postgres");
         stm = connection.createStatement();
     }
@@ -36,12 +36,12 @@ public class DatabaseMethods {
     private static void readAndSetNewStudentScore(String pathFile) throws FileNotFoundException {
         Scanner scanner = new Scanner(new FileInputStream(pathFile));
         ArrayList<String[]> studentsAllInfo = new ArrayList<>();
-        while (scanner.hasNextLine()){
-            studentsAllInfo.add(scanner.nextLine().split(", "));
+        while (scanner.hasNextLine()) {
+            studentsAllInfo.add(scanner.nextLine().split("  "));
         }
-        for (String[] student: studentsAllInfo) {
+        for (String[] student : studentsAllInfo) {
             try {
-                updateStudentScore(Integer.parseInt(student[0]), Integer.parseInt(student[2]));
+                updateStudentScore(student[0], student[2]);
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -51,22 +51,23 @@ public class DatabaseMethods {
 
     private static void findAllStudents() throws SQLException {
         ResultSet rs = stm.executeQuery("SELECT * FROM students");
-        while (rs.next()){
+        while (rs.next()) {
             System.out.println(rs.getInt(1) + " " + rs.getInt(2) + " " + rs.getString(3));
         }
         System.out.println();
     }
 
-    private static void insertNewStudent(int age, String name) throws SQLException {
-        stm.executeUpdate(String.format("INSERT INTO students (age_fld, name_fld) values(%d, '%s');", age, name));
+    private static void insertNewStudent(int score, String name) throws SQLException {
+        stm.executeUpdate(String.format("INSERT INTO students (score_fld, name_fld) values(%d, '%s');", score, name));
     }
 
-    private static void updateStudent(int id, int updateAge, String name) throws SQLException {
-        stm.executeUpdate(String.format("UPDATE students SET age_fld = %d, name_fld = '%s' WHERE student_id = %d", updateAge, name, id));
+    private static void updateStudent(int id, int updateScore, String name) throws SQLException {
+        stm.executeUpdate(String.format("UPDATE students SET score_fld = %d, name_fld = '%s' WHERE student_id = %d;", updateScore, name, id));
     }
 
-    private static void updateStudentScore(int id, int score) throws SQLException {
-        stm.executeUpdate(String.format("UPDATE students SET score_fld = %d WHERE student_id = %d", score, id));
+    private static void updateStudentScore(String id, String score) throws SQLException {
+        String req = String.format("UPDATE public.students SET score_fld = %s WHERE student_id = %s;", score, id);
+        stm.executeUpdate(req);
     }
 
     private static void deleteStudent(int id) throws SQLException {
