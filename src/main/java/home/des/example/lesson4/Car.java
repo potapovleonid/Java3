cf1e71c232b10ffb1bce90d47cb43021b130c5cc
Leonid <kissmykrabeskill@gmail.com>
2022-10-01 22:21:01 +0500

    Added start together

--- src/main/java/home/des/example/lesson4/Car.java
+++ src/main/java/home/des/example/lesson4/Car.java
@@ -1,5 +1,7 @@
 package home.des.example.lesson4;
 
+import java.util.concurrent.CountDownLatch;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
 
@@ -7,6 +9,8 @@ public class Car implements Runnable {
         CARS_COUNT = 0;
     }
 
+    private CountDownLatch readyCountDown;
+    private CountDownLatch finishCountDown;
     private Race race;
     private int speed;
     private String name;
@@ -19,11 +23,13 @@ public class Car implements Runnable {
         return speed;
     }
 
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CountDownLatch readyCountDown, CountDownLatch finishCountDown) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.readyCountDown = readyCountDown;
+        this.finishCountDown = finishCountDown;
     }
 
     @Override
@@ -32,11 +38,15 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int) (Math.random() * 800));
             System.out.println(this.name + " готов");
+            readyCountDown.countDown();
+            readyCountDown.await();
+            Thread.sleep(1000);
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+        finishCountDown.countDown();
     }
 }
