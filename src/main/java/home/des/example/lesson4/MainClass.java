72e2487291051de755fb3b94f317802be2937b9e
Leonid <kissmykrabeskill@gmail.com>
2022-10-02 18:29:12 +0500

    Added start together using CyclicBarrier

--- src/main/java/home/des/example/lesson4/MainClass.java
+++ src/main/java/home/des/example/lesson4/MainClass.java
@@ -1,13 +1,23 @@
 package home.des.example.lesson4;
 
+import sun.applet.Main;
+
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 
 public class MainClass {
     public static final int CARS_COUNT = 4;
-    public static CountDownLatch readyCountDown = new CountDownLatch(CARS_COUNT);
-    public static CountDownLatch finishCountDown = new CountDownLatch(CARS_COUNT);
 
     public static void main(String[] args) {
+//        MainClass.countDownRace();
+        MainClass.cyclicRace();
+    }
+
+    public static void countDownRace(){
+        CountDownLatch readyCountDown = new CountDownLatch(CARS_COUNT);
+        CountDownLatch finishCountDown = new CountDownLatch(CARS_COUNT);
+
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
@@ -30,6 +40,38 @@ public class MainClass {
         try {
             finishCountDown.await();
         } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
+
+    public static void cyclicRace(){
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(CARS_COUNT + 1);
+
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cyclicBarrier);
+        }
+
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
+        }
+
+        try {
+            cyclicBarrier.await();
+            cyclicBarrier.reset();
+        } catch (BrokenBarrierException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+
+        try {
+            cyclicBarrier.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
             e.printStackTrace();
         }
 
